2bb444ce17de27d8415cf2428c7ee6dce51817f0
João Monteiro <monteiro_1998@hotmail.com>
2025-03-28 17:55:34 +0000

    login inital form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {faEnvelope,faLock} from '@fortawesome/free-solid-svg-icons';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    FaIconComponent
+  ],
   templateUrl: './login.component.html',
+  standalone: true,
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  faEnvelope = faEnvelope;
+  faLock = faLock;
 }
