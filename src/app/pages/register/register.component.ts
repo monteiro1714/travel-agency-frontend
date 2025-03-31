8ad4fb93e82b1be8a89f8f0d6f6278f9ab1bfb32
João Monteiro <monteiro_1998@hotmail.com>
2025-03-31 21:07:45 +0100

    Form examples login/register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    FaIconComponent,
+    RouterLink
+  ],
   templateUrl: './register.component.html',
+  standalone: true,
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  faEnvelope = faEnvelope;
+  faLock = faLock;
 
 }
